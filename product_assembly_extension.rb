e6c7eb46ad09e7d31e0ec4ccde76a6701abdaeb9
Roman Smirnov <roman@railsdog.com>
2009-10-31 18:02:19 +0300

    Override work with inventory units, if product is assembly.

--- product_assembly_extension.rb
+++ product_assembly_extension.rb
@@ -34,6 +34,30 @@ class ProductAssemblyExtension < Spree::Extension
             :foreign_key => "assembly_id", :association_foreign_key => "part_id"
 
 
+      alias_method :orig_on_hand, :on_hand
+      # returns the number of inventory units "on_hand" for this product
+      def on_hand
+        if self.assembly?
+          parts.map{|v| v.on_hand / self.count_of(v) }.min
+        else
+          orig_on_hand
+        end
+      end
+
+      alias_method :orig_on_hand=, :on_hand=
+      def on_hand=(new_level)
+        orig_on_hand=(new_level) unless self.assembly?
+      end
+
+      alias_method :orig_has_stock?, :has_stock?
+      def has_stock?
+        if self.assembly?
+          !parts.detect{|v| self.count_of(v) > v.on_hand}
+        else
+          orig_has_stock?
+        end
+      end
+
       def add_part(variant, count = 1)
         begin
           ap = AssembliesPart.find(self.id, variant.id)        
@@ -88,6 +112,42 @@ class ProductAssemblyExtension < Spree::Extension
         end         
       end
 
+    end
+    
+    
+    InventoryUnit.class_eval do
+      def self.sell_units(order)
+        order.line_items.each do |line_item|
+          variant = line_item.variant
+          quantity = line_item.quantity
+
+          product = variant.product
+          if product.assembly?
+            product.parts.each do |v|
+              on_hand_units = self.retrieve_on_hand(v, quantity * product.count_of(v))
+              mark_units_as_selled(order, on_hand_units)
+            end
+          else
+            on_hand_units = self.retrieve_on_hand(variant, quantity)
+            mark_units_as_selled(order, on_hand_units)
+          end
+        end
+      end
+      
+      private
+      
+      def self.mark_units_as_selled(order, units)       
+        # mark all of these units as sold and associate them with this order 
+        units.each do |unit|          
+          unit.order = order
+          unit.sell!
+        end
+        # right now we always allow back ordering
+        backorder = quantity - units.size
+        backorder.times do 
+          order.inventory_units.create(:variant => variant, :state => "backordered")
+        end      
+      end
     end
     
   end
