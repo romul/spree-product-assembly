a6419eaae1ecbba95fa5d55b31b2e5ae97352e72
Roman Smirnov <roman@railsdog.com>
2009-11-10 23:38:20 +0300

    Rewrite js-code to be unobtrusive.

---
+++ public/javascripts/product_assembly.js
@@ -0,0 +1,37 @@
+  $("#searchtext").keypress(function (e) {  
+    if ((e.which && e.which == 13) || (e.keyCode && e.keyCode == 13)) {  
+      search_for_parts();
+      return false;  
+    } else {  
+       return true;  
+    }  
+  });
+  
+  $("#searchtext").delayedObserver(0.75, function(element, value) {
+    search_for_parts();
+  })
+  
+  function subscribe_product_part_links()
+  {
+    $("a.set_count_admin_product_part_link").click(function(){
+      params = { count :  $("input", $(this).parent().parent()).val() };
+      return make_post_request($(this), params);
+    });
+    
+    $("a.remove_admin_product_part_link").click(function(){
+      return make_post_request($(this), {});
+    });
+  }
+  
+  function make_post_request(link, post_params)
+  {
+    spinner = $("img.spinner", link.parent())
+    spinner.show();
+    $.post(link.attr("href"), post_params,
+				function (data, textStatus) { spinner.hide(); },
+				"script");
+
+    return false;    
+  }
+  
+  subscribe_product_part_links();  
