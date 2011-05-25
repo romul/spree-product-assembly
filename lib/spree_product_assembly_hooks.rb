98e977c9a071040a26a7dc9ed159e6ba53fc9148
Roman Smirnov <roman@railsdog.com>
2011-05-25 15:05:04 +0400

    Switch to Deface

--- lib/spree_product_assembly_hooks.rb
+++ lib/spree_product_assembly_hooks.rb
@@ -1,9 +1,18 @@
-class SpreeProductAssemblyHooks < Spree::ThemeSupport::HookListener
+Deface::Override.new(:virtual_path => "admin/shared/_product_tabs",
+                     :name => "product_assembly_admin_product_tabs",
+                     :insert_bottom => "[data-hook='admin_product_tabs']",
+                     :partial => "admin/shared/product_assembly_product_tabs",
+                     :disabled => false)
 
-  insert_after :admin_product_tabs, "admin/shared/product_assembly_product_tabs"
+Deface::Override.new(:virtual_path => "orders/_line_item",
+                     :name => "product_assembly_cart_item_description",
+                     :insert_bottom => "[data-hook='cart_item_description']",
+                     :partial => "orders/cart_description",
+                     :disabled => false)
 
-  insert_after :cart_item_description, "orders/cart_description"
-
-  insert_after :admin_product_form_right , "admin/products/product_assembly_fields"
-
-end
+Deface::Override.new(:virtual_path => "admin/products/_form",
+                     :name => "product_assembly_admin_product_form_right",
+                     :insert_after => "[data-hook='admin_product_form_right'], #admin_product_form_right[data-hook]",
+                     :partial => "admin/products/product_assembly_fields",
+                     :disabled => false)
+                     
