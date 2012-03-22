dd22a5a8ba4ad36d88c0ecae17b620424f073255
Joe <cpmhjoe@gmail.com>
2012-03-22 03:34:41 -0400

    Updated for spree 1.0.x

--- app/overrides/add_admin_product_form_fields.rb
+++ app/overrides/add_admin_product_form_fields.rb
@@ -1,4 +1,4 @@
-Deface::Override.new(:virtual_path => "admin/products/_form",
+Deface::Override.new(:virtual_path => "spree/admin/products/_form",
                      :name => "product_assembly_admin_product_form_right",
                      :insert_after => "[data-hook='admin_product_form_right'], #admin_product_form_right[data-hook]",
                      :partial => "spree/admin/products/product_assembly_fields",
--- app/overrides/add_admin_tabs.rb
+++ app/overrides/add_admin_tabs.rb
@@ -1,4 +1,4 @@
-Deface::Override.new(:virtual_path => "admin/shared/_product_tabs",
+Deface::Override.new(:virtual_path => "spree/admin/shared/_product_tabs",
                      :name => "product_assembly_admin_product_tabs",
                      :insert_bottom => "[data-hook='admin_product_tabs']",
                      :partial => "spree/admin/shared/product_assembly_product_tabs",
