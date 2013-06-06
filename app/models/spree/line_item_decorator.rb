37b82d1ec7e7cf57c1bddb99fd726502c3ede02a
Washington Luiz <huoxito@gmail.com>
2013-06-06 14:52:07 -0300

    Spree::Variant no longer has a `on_hand` attribute
    Instead stock it should be check through variant stock items. This makes
    it possible to go through checkout without raising errors

--- app/models/spree/line_item_decorator.rb
+++ app/models/spree/line_item_decorator.rb
@@ -1,37 +1,19 @@
 Spree::LineItem.class_eval do
-  
   validate :validate_quantity_and_stock
 
   private
-
-  def validate_quantity_and_stock
-    unless quantity && quantity >= 0
-      errors.add(:quantity, I18n.t("validation.must_be_non_negative"))
-    end
-    # avoid reload of order.inventory_units by using direct lookup
-    unless !Spree::Config[:track_inventory_levels]                        ||
-           Spree::Config[:allow_backorders]                               ||
-           order   && Spree::InventoryUnit.where(:order_id => order.id).first.present? ||
-           variant && quantity <= variant.on_hand
-      errors.add(:quantity, I18n.t("validation.is_too_large") + " (#{self.variant.name})")
-    end
-
-    return unless variant
-    
-=begin # Commented out until Order#shipped_units not restored in the Core
-    if variant.product.assembly?
-      variant.product.parts.each do |part|
-        if shipped_count = order.shipped_units.nil? ? nil : order.shipped_units[part]
-          errors.add(:quantity, I18n.t("validation.cannot_be_less_than_shipped_units") ) if quantity < shipped_count
-        end
+    def validate_quantity_and_stock
+      unless quantity && quantity >= 0
+        errors.add(:quantity, I18n.t("validation.must_be_non_negative"))
       end
-    else
-      if shipped_count = order.shipped_units.nil? ? nil : order.shipped_units[variant]
-        errors.add(:quantity, I18n.t("validation.cannot_be_less_than_shipped_units") ) if quantity < shipped_count
+
+      # avoid reload of order.inventory_units by using direct lookup
+      unless !Spree::Config[:track_inventory_levels] || quantity < variant_stock_on_hand
+        errors.add(:quantity, I18n.t("validation.is_too_large") + " (#{self.variant.name})")
       end
     end
-=end
-    
-  end
 
+    def variant_stock_on_hand
+      variant.stock_items.sum(&:count_on_hand)
+    end
 end
