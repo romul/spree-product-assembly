693aaf15964f4cb2f9c861db8047e342392fc907
Washington Luiz <huoxito@gmail.com>
2013-06-12 10:54:25 -0300

    Check for parts stock items instead if product is assembly

--- app/models/spree/stock/packer_decorator.rb
+++ app/models/spree/stock/packer_decorator.rb
@@ -15,12 +15,6 @@ module Spree
       def product_assembly_package
         package = Package.new(stock_location, order)
         order.line_items.each do |line_item|
-          next unless stock_location.stock_item(line_item.variant)
-
-          on_hand, backordered = stock_location.fill_status(line_item.variant, line_item.quantity)
-          package.add line_item.variant, on_hand, :on_hand if on_hand > 0
-          package.add line_item.variant, backordered, :backordered if backordered > 0
-
           product = line_item.product
           if product.assembly?
             product.parts.each do |part|
@@ -30,6 +24,12 @@ module Spree
               package.add part, on_hand, :on_hand if on_hand > 0
               package.add part, backordered, :backordered if backordered > 0
             end
+          else
+            next unless stock_location.stock_item(line_item.variant)
+
+            on_hand, backordered = stock_location.fill_status(line_item.variant, line_item.quantity)
+            package.add line_item.variant, on_hand, :on_hand if on_hand > 0
+            package.add line_item.variant, backordered, :backordered if backordered > 0
           end
         end
         package
