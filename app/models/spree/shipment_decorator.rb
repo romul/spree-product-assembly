1f14ce4f24b37c617e1d419c2938088132033560
Washington Luiz <huoxito@gmail.com>
2013-06-14 16:55:24 -0300

    Resolve conflich between individual product and bundle part
    Spree should know whether the inventory unit is associated with a
    product sold individually or with a part of a bundle.
    
    We probably need to also add a part column on the InventoryUnit table so
    that this behaviour is persisted at the unit level doesn't depend on the
    bundle product state which might change in the future

--- app/models/spree/shipment_decorator.rb
+++ app/models/spree/shipment_decorator.rb
@@ -1,11 +1,30 @@
 module Spree
   Shipment.class_eval do
     def manifest
-      inventory_units.group_by(&:line_item).map do |line_item, units|
+      inventory_units.includes(:line_item, :variant).group_by(&:line_item).map do |line_item, units|
         states = {}
         units.group_by(&:state).each { |state, iu| states[state] = iu.count }
         OpenStruct.new(line_item: line_item, variant: line_item.variant, quantity: units.length, states: states)
       end
+    end
+
+    def variant_manifest
+      items = []
+      inventory_units.includes(:variant, :line_item).group_by(&:variant).each do |variant, units|
+        states = {}
+
+        units.group_by(&:line_item).each do |line_item, units|
+          units.group_by(&:state).each { |state, iu| states[state] = iu.count }
+          part = line_item.product.assembly?
+          items << OpenStruct.new(part: part,
+                                  product: line_item.product,
+                                  line_item: line_item,
+                                  variant: variant,
+                                  quantity: units.length,
+                                  states: states)
+        end
+      end
+      items
     end
   end
 end
