26a3602f556dddb4a6a0dacd84379c9eb583f2b3
Washington Luiz <huoxito@gmail.com>
2013-06-15 23:05:35 -0300

    Improve test coverage
    Remove repeated spec and ensure shipment manifest behave as expected on
    this extension

--- spec/models/spree/shipment_spec.rb
+++ spec/models/spree/shipment_spec.rb
@@ -2,7 +2,39 @@ require 'spec_helper'
 
 module Spree
   describe Shipment do
-    pending "make sure shipment.manifest returns units grouped properly to unsure proper stock item update"
-    pending "account for situation where units doesn't have a line item"
+    include_context "product is ordered as individual and within a bundle"
+
+    let(:inventory_unit) { create(:inventory_unit) }
+
+    context "manifests" do
+      let(:shipments) { order.create_proposed_shipments }
+
+      context "default" do
+        let(:expected_variants) { order.variants - [bundle_variant] + bundle.parts }
+
+        it "separates variant purchased individually from the bundle one" do
+          expect(shipments.count).to eql 1
+          shipments.first.manifest.map(&:variant).sort.should == expected_variants.sort
+        end
+      end
+
+      context "line items manifest" do
+        let(:expected_variants) { order.variants }
+
+        it "groups units by line_item only" do
+          expect(shipments.count).to eql 1
+          shipments.first.line_item_manifest.map(&:variant).sort.should == expected_variants.sort
+        end
+      end
+
+      context "units are not associated with a line item" do
+        let(:order) { create(:shipped_order) }
+        let(:shipment) { order.shipments.first }
+
+        it "searches for line item if inventory unit doesn't have one" do
+          shipment.manifest.last.line_item.should_not be_blank
+        end
+      end
+    end
   end
 end
--- spec/models/spree/stock/coordinator_spec.rb
+++ spec/models/spree/stock/coordinator_spec.rb
@@ -3,34 +3,21 @@ require 'spec_helper'
 module Spree
   module Stock
     describe Coordinator do
-      let(:order) { create(:order_with_line_items) }
-      let(:parts) { (1..3).map { create(:variant) } }
+      include_context "product is ordered as individual and within a bundle"
 
-      let(:bundle_variant) { order.variants.first }
-      let(:bundle) { bundle_variant.product }
+      subject { Coordinator.new(order) }
 
-      context "a product is ordered both as individual and within a bundle" do
-        let(:common_product) { order.variants.last }
+      before { StockItem.update_all 'count_on_hand = 10' }
 
-        subject { Coordinator.new(order) }
+      context "bundle part requires more units than individual product" do
+        before { order.contents.add(bundle_variant, 5) }
 
-        before do
-          expect(bundle_variant).to_not eql common_product
+        let(:bundle_item_quantity) { order.find_line_item_by_variant(bundle_variant).quantity }
 
-          bundle.parts << [parts, common_product]
-          StockItem.update_all 'count_on_hand = 10'
-        end
+        it "calculates items quantity properly" do
+          expected_units_on_package = order.line_items.sum(&:quantity) - bundle_item_quantity + (bundle.parts.count * bundle_item_quantity)
 
-        context "bundle part requires more units than individual product" do
-          before { order.contents.add(bundle_variant, 5) }
-
-          let(:bundle_item_quantity) { order.line_items.where(variant_id: bundle_variant.id).first.quantity }
-
-          it "calculates items quantity properly" do
-            expected_units_on_package = order.line_items.sum(&:quantity) - bundle_item_quantity + (bundle.parts.count * bundle_item_quantity)
-
-            expect(subject.packages.sum(&:quantity)).to eql expected_units_on_package
-          end
+          expect(subject.packages.sum(&:quantity)).to eql expected_units_on_package
         end
       end
     end
