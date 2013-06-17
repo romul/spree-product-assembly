42069636c3406d6b3239e47d7adcc9e78672126b
Washington Luiz <huoxito@gmail.com>
2013-06-17 02:30:22 -0300

    Use OpenStruct to build package content
    Help to get rid of initialized constant warning when assign a Struct to
    new constant on the package decorator

--- app/models/spree/stock/package_decorator.rb
+++ app/models/spree/stock/package_decorator.rb
@@ -1,34 +1,32 @@
 module Spree
   module Stock
     Package.class_eval do
-      AssemblyContentItem = Struct.new(:variant, :quantity, :state, :line_item)
-
-      # TODO Maybe we don't need to override this method
+      # Overriden from Spree core
       #
-      # Instead we could add a `package.add_bundle_part` or something like
-      # that so that we could use this while adding parts of a bundle 
-      # and the spree core `package.add` to add invidual (regular) products.
-      # This will make the extension play way more well with Spree coder
-      # default internals. Just need to keep in mind that to do that we need
-      # make the ContentItem respond to `line_item`
+      # Package contents need to have a line item as well so we can tell
+      # whether the item comes a product assembly or from a regular product
+      #
+      # Returns the updated contents array
       def add(variant, quantity, state=:on_hand, line_item = nil)
-        contents << AssemblyContentItem.new(variant, quantity, state, line_item)
+        contents << OpenStruct.new(variant: variant, quantity: quantity, state: state, line_item: line_item)
       end
 
+      # Overriden from Spree core
+      #
       # When searching for an item it needs to check also if line item matches
       # otherwise the first item found may not be exactly the one you'd expect
       #
       # e.g.
-      # Customer ordered three bundles and one individual regular product
+      # Customer ordered 3 bundles and 1 individual regular product
       # The bundle includes the product ordered individually as a part
       #
-      # When searching for the item it may will not distinguish the product
+      # When searching for the item it may not distinguish the product
       # from the part, as they really the same variant. The side effect is
       # that the Priotirizer or the Adjuster may increase / decrease the
-      # quantity of the item as it will believe that the package has more
-      # than the variant on the order actually needs.
+      # quantity of the item as it will believe that the package has more,
+      # or less, than the variant on the order actually needs.
       #
-      # Following the example above the prioritizer may remove two of the
+      # Following the example above the prioritizer may remove 2 of the
       # bundle parts because it would think that the order actually only
       # needs one (the quantity customer ordered for the individual product),
       # the Coordinator spec has a use case for that
