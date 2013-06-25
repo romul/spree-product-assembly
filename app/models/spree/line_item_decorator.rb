accc0b8a7149dabd6d43ce3b019c5aa58635e05f
Washington Luiz <huoxito@gmail.com>
2013-06-25 17:14:22 -0300

    Create OrderIventoryAssembly class
    Make it easier to read the code and realize what's going on, decreasing
    product bundle quantity still not working properly

--- app/models/spree/line_item_decorator.rb
+++ app/models/spree/line_item_decorator.rb
@@ -1,5 +1,14 @@
 module Spree
   LineItem.class_eval do
     scope :assemblies, -> { joins(:product => :parts).uniq }
+
+    private
+      def update_inventory
+        if self.product.assembly? && order.completed?
+          OrderInventoryAssembly.new(self).verify(target_shipment)
+        else
+          OrderInventory.new(self.order).verify(self, target_shipment)
+        end
+      end
   end
 end
---
+++ app/models/spree/order_inventory_assembly.rb
@@ -0,0 +1,107 @@
+module Spree
+  class OrderInventoryAssembly
+    attr_reader :order, :line_item, :product
+
+    def initialize(line_item)
+      @order = line_item.order
+      @line_item = line_item
+      @product = line_item.product
+    end
+
+    def verify(shipment = nil)
+      parts_total = product.assemblies_parts.sum(&:count)
+
+      if inventory_units.size < (parts_total * line_item.quantity)
+
+        product.parts.each do |part|
+          quantity = (product.count_of(part) * (line_item.quantity - line_item.changed_attributes['quantity']))
+
+          shipment = determine_target_shipment(part) unless shipment
+          add_to_shipment(shipment, part, quantity)
+        end
+      elsif inventory_units.size > (parts_total * line_item.quantity)
+        remove(shipment)
+      end
+    end
+
+    def inventory_units
+      units = order.shipments.collect { |s| s.inventory_units.all }.flatten
+      @inventory_units ||= units.group_by(&:line_item)[line_item.id] || []
+    end
+
+    private
+      def remove(shipment = nil)
+        product.parts.each do |part|
+          quantity = (product.count_of(part) * (line_item.changed_attributes['quantity'] - line_item.quantity))
+
+          if shipment.present?
+            remove_from_shipment(shipment, part, quantity)
+          else
+            order.shipments.each do |shipment|
+              break if quantity == 0
+              quantity -= remove_from_shipment(shipment, part, quantity)
+            end
+          end
+        end
+      end
+
+      # Returns either one of the shipment:
+      #
+      # first unshipped that already includes this variant
+      # first unshipped that's leaving from a stock_location that stocks this variant
+      #
+      def determine_target_shipment(variant)
+        shipment = order.shipments.detect do |shipment|
+          (shipment.ready? || shipment.pending?) && shipment.include?(variant)
+        end
+
+        shipment ||= order.shipments.detect do |shipment|
+          (shipment.ready? || shipment.pending?) && variant.stock_location_ids.include?(shipment.stock_location_id)
+        end
+      end
+
+      # create inventory_units
+      # adding to this shipment, and removing from stock_location
+      # return quantity added
+      def add_to_shipment(shipment, variant, quantity)
+        on_hand, back_order = shipment.stock_location.fill_status(variant, quantity)
+
+        on_hand.times do
+          shipment.inventory_units.create({line_item: line_item, variant: variant, state: 'on_hand'}, without_protection: true)
+        end
+
+        back_order.times do
+          shipment.inventory_units.create({line_item: line_item, variant: variant, state: 'backordered'}, without_protection: true)
+        end
+
+        shipment.stock_location.unstock variant, quantity, shipment
+        quantity
+      end
+
+      def remove_from_shipment(shipment, variant, quantity)
+        return 0 if quantity == 0 || shipment.shipped?
+
+        shipment_units = shipment.inventory_units_for(variant).reject do |variant_unit|
+          variant_unit.state == 'shipped'
+        end.sort_by(&:state)
+
+        removed_quantity = 0
+
+        shipment_units.each do |inventory_unit|
+          break if removed_quantity == quantity
+          inventory_unit.destroy
+          removed_quantity += 1
+        end
+
+        if shipment.inventory_units.count == 0
+          shipment.destroy
+        end
+
+        # removing this from shipment, and adding to stock_location
+        shipment.stock_location.restock variant, removed_quantity, shipment
+
+        # return quantity removed
+        removed_quantity
+      end
+  end
+end
