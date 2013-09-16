df156267b4acc28f54f6d3a4dfb7e1b354b4b485
Roy <Maysora@users.noreply.github.com>
2013-09-16 21:03:42 +0700

    fix syntax error

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -12,7 +12,7 @@ Spree::Product.class_eval do
 
   scope :individual_saled, -> { where(["spree_products.individual_sale = ?", true]) }
 
-  scope :search_can_be_part, -> (query) { not_deleted.available.joins(:master)
+  scope :search_can_be_part, ->(query){ not_deleted.available.joins(:master)
     .where(arel_table["name"].matches("%#{query}%").or(Spree::Variant.arel_table["sku"].matches("%#{query}%")))
     .where(can_be_part: true)
     .limit(30)
