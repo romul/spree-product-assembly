e4b5aba836b41219bdc540d4096e43ef2bbf1ff6
Washington Luiz <huoxito@gmail.com>
2013-12-16 00:48:55 -0300

    Dont require spree/frontend in gemspec

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -1,7 +1,9 @@
 module Spree
-  CheckoutController.class_eval do
-    # Override because we don't want to remove unshippable items from the order
-    # A bundle itself is an unshippable item
-    def before_payment; end
+  if defined? Spree::Frontend
+    CheckoutController.class_eval do
+      # Override because we don't want to remove unshippable items from the order
+      # A bundle itself is an unshippable item
+      def before_payment; end
+    end
   end
 end
