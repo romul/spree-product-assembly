64ae2718b06f55111ab7a34a1015f0ea6657e8d2
Kian Wright <wright_kian@yahoo.com>
2014-02-04 22:28:28 -0800

    Packer now tests whether it should track inventory. Spree::Stock::Packer.product_assembly_package tests whether inventory is being tracked, as Spree's #default_package does. If inventory is not being tracked, the part or variant is added with an :on_hand status. Specs included.

--- spec/models/spree/stock/packer_spec.rb
+++ spec/models/spree/stock/packer_spec.rb
@@ -15,6 +15,7 @@ module Spree
           packages.size.should eq 1
           packages.first.contents.size.should eq 5
         end
+        
       end
 
       context 'build bundle product package' do
@@ -24,12 +25,12 @@ module Spree
           order.products.last.parts << parts
         end
 
-        it 'adds all bundle parts to the shipent' do
+        it 'adds all bundle parts to the shipment' do
           package = subject.product_assembly_package
           package.contents.size.should eq 4 + parts.count
         end
 
-        context "order has backorered and on hand items" do
+        context "order has backordered and on hand items" do
           before do
             stock_item = stock_location.stock_item(parts.first)
             stock_item.adjust_count_on_hand(10)
@@ -37,6 +38,53 @@ module Spree
 
           it "splits package in two as expected (backordered, on_hand)" do
             expect(subject.packages.count).to eql 2
+          end
+        end
+
+        context "store doesn't track inventory" do
+          before { Spree::Config.track_inventory_levels = false }
+          
+          it 'adds items as on-hand, not backordered' do
+            stock_item = stock_location.stock_item(order.line_items.first)
+            
+            package = subject.product_assembly_package
+            package.contents.size.should eq 4 + parts.count
+            package.contents.each {|ci| ci.state.should eq :on_hand}
+          end
+        end
+      
+        context "are tracking inventory" do
+          before do
+            Spree::Config.track_inventory_levels = true
+            # by default, variant factory sets track_inventory to true
+          end
+          
+          it 'adds items as backordered' do
+            package = subject.product_assembly_package
+            package.contents.size.should eq 4 + parts.count
+            package.contents.each {|ci| ci.state.should eq :backordered}
+          end
+        end
+      
+        context 'variants and parts do not track inventory' do
+          before(:each) do
+            Spree::Config.track_inventory_levels = true
+            order.line_items.each do |li| 
+              li.variant.track_inventory = false
+              li.save!
+              if li.product.assembly?
+                li.product.parts.each do |part|
+                  part.track_inventory = false
+                  part.save!
+                end
+              end
+            end
+          end
+          
+          it 'adds items as on-hand, not backordered' do
+            package = subject.product_assembly_package
+            package.contents.size.should eq 4 + parts.count
+            package.contents.each {|ci| ci.state.should eq :on_hand}
           end
         end
       end
