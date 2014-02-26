61a93a0934db03efeb4d8de025f86225282f1875
Roman Smirnov <POMAHC@gmail.com>
2014-02-26 19:26:01 +0400

    Remove debug-message

--- app/models/spree/price_decorator.rb
+++ app/models/spree/price_decorator.rb
@@ -4,7 +4,6 @@ Spree::Price.class_eval do
   private
   
   def recalc_assemblies_price
-    puts "Spree::Price.after_update"
     return unless amount_changed?
     return unless Spree::ProductAssembly::Config[:auto_recalc_assemblies_price]
 
