46da83e954933c7073f14a124df27d1d72950e60
Tobias Bohwalli <hi@futhr.io>
2014-03-26 18:38:55 +0100

    Update dev dependencies + cleanup.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,15 @@
-# Configure Rails Environment
+require 'simplecov'
+SimpleCov.start 'rails'
+
 ENV["RAILS_ENV"] = "test"
 
 require File.expand_path("../dummy/config/environment.rb",  __FILE__)
+
 require 'rspec/rails'
 require 'ffaker'
+require 'capybara/rspec'
+require 'capybara/rails'
 
-# Requires supporting ruby files with custom matchers and macros, etc,
-# in spec/support/ and its subdirectories.
 Dir[File.join(File.dirname(__FILE__), "support/**/*.rb")].each {|f| require f }
 
 require 'spree/testing_support/factories'
@@ -14,14 +17,11 @@ require 'spree/testing_support/url_helpers'
 require 'spree/testing_support/authorization_helpers'
 
 RSpec.configure do |config|
-  config.color = true
   config.mock_with :rspec
-
   config.use_transactional_fixtures = true
 
   config.include FactoryGirl::Syntax::Methods
-
-  config.extend Spree::TestingSupport::AuthorizationHelpers::Request, type: :feature
+  config.include Spree::TestingSupport::UrlHelpers
 end
 
 class ActiveRecord::Base
