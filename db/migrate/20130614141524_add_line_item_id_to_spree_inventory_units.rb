11c136e1a783dc05b963a87fba0e6db074a43331
Michael Lippold <michael@trialguides.com>
2015-02-12 15:12:43 -0800

    Removed migration for column added to Spree 2.2

--- db/migrate/20130614141524_add_line_item_id_to_spree_inventory_units.rb
+++
@@ -1,6 +0,0 @@
-class AddLineItemIdToSpreeInventoryUnits < ActiveRecord::Migration
-  def change
-    add_column :spree_inventory_units, :line_item_id, :integer
-    add_index :spree_inventory_units, :line_item_id
-  end
-end
